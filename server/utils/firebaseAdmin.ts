5d521de519c76ac1bd1f6ef8f2b962ede081de32
prabowo <anthonyprabowo@gmail.com>
2025-11-20 10:44:10 -0800

    change service account to be in env instead

--- server/utils/firebaseAdmin.ts
+++ server/utils/firebaseAdmin.ts
@@ -1,13 +1,24 @@
 // server/utils/firebaseAdmin.ts
 import { initializeApp, cert, getApps } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
-import serviceAccount from './minty-budget-app-firebase-adminsdk-fbsvc-0fa2dba133.json'
 import { getFirestore } from 'firebase-admin/firestore'
+
+function getServiceAccount() {
+  const raw = process.env.NUXT_FB_CREDENTIALS
+  if (!raw) {
+      throw new Error('Missing FIREBASE_SERVICE_ACCOUNT_JSON in environment variables')
+  }
+  const serviceAccount = JSON.parse(raw)
+  if (serviceAccount.private_key) {
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n')
+  }
+  return serviceAccount
+}
 
 const app = getApps().length
   ? getApps()[0]
   : initializeApp({
-      credential: cert(serviceAccount as any),
+      credential: cert(getServiceAccount() as any),
     })
 
 export const adminAuth = () => getAuth(app)
